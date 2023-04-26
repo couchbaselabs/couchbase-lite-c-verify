a37ab9685f3f52265f2186127bd3653575137e09
Pasin Suriyentrakorn <pasin@couchbase.com>
2023-04-26 14:33:03 -0700

    Add linux build script and workflow

---
+++ scripts/verify_linux.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -ex
+
+# Global define
+VERSION=${1}
+EDITION=${2}
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+CBL_LIB_DIR=$SCRIPT_DIR/../lib
+BUILD_DIR=$SCRIPT_DIR/../build
+
+rm -rf $CBL_LIB_DIR 2> /dev/null
+mkdir -p $CBL_LIB_DIR
+pushd $CBL_LIB_DIR
+
+ZIP_FILENAME=couchbase-lite-c-${EDITION}-${VERSION}-linux-x86_64.tar.gz
+curl -O https://packages.couchbase.com/releases/couchbase-lite-c/${VERSION}/${ZIP_FILENAME}
+tar xvf ${ZIP_FILENAME}
+rm ${ZIP_FILENAME}
+
+popd
+mkdir -p $BUILD_DIR
+pushd $BUILD_DIR
+
+cmake -DCMAKE_PREFIX_PATH=$CBL_LIB_DIR/libcblite-$VERSION -DCMAKE_BUILD_TYPE=Release ..
+make
+
+./verify
