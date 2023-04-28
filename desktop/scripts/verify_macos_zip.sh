0089c2fe2480dadef1aaded0fa4a4732c07a421e
Pasin Suriyentrakorn <pasin@couchbase.com>
2023-04-28 16:39:05 -0700

    Add workflow for iOS

--- desktop/scripts/verify_macos_zip.sh
+++ desktop/scripts/verify_macos_zip.sh
@@ -1,8 +1,12 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
-# Global define
-VERSION=${1}
-EDITION=${2}
+if [ "$#" -lt 2 ]; then
+    echo "Usage: verify_macos_zip.sh <version: 3.1.0> <edition: community, enterprise>" >&2
+    exit 1
+fi
+
+VERSION=$1
+EDITION=$2
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 CBL_LIB_DIR=$SCRIPT_DIR/../lib
@@ -10,18 +14,18 @@ BUILD_DIR=$SCRIPT_DIR/../build
 
 rm -rf $CBL_LIB_DIR 2> /dev/null
 mkdir -p $CBL_LIB_DIR
-pushd $CBL_LIB_DIR
-
+pushd $CBL_LIB_DIR > /dev/null
 ZIP_FILENAME=couchbase-lite-c-${EDITION}-${VERSION}-macos.zip
 curl -O https://packages.couchbase.com/releases/couchbase-lite-c/${VERSION}/${ZIP_FILENAME}
 unzip ${ZIP_FILENAME}
 rm ${ZIP_FILENAME}
+popd > /dev/null
 
-popd
 mkdir -p $BUILD_DIR
-pushd $BUILD_DIR
+pushd $BUILD_DIR > /dev/null
 
 cmake -DCMAKE_PREFIX_PATH=$CBL_LIB_DIR/libcblite-$VERSION -DCMAKE_BUILD_TYPE=Release ..
 make
-
 ./verify
+
+popd > /dev/null
