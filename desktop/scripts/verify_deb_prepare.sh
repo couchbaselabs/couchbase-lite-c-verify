b6b5d9e2edcd7c54098980cd7ec7193b10d1d275
Pasin Suriyentrakorn <pasin@couchbase.com>
2023-06-27 19:18:40 -0700

    Fix docker command bug and etc (#9)
    * Explicitly set the env instead of using -e when running docker compose (-e is for docker run command).
    
    * Update docker-compose.yml to remove unsupported platform for 3.0.x.
    
    * Updated verify_deb_prepare.sh script
    
    * Updated README

--- desktop/scripts/verify_deb_prepare.sh
+++ desktop/scripts/verify_deb_prepare.sh
@@ -10,7 +10,7 @@ ARCH=$1
 if [ $ARCH = "armhf" ]; then
     dpkg --add-architecture armhf
     apt -yqq update
-    DEBIAN_FRONTEND=noninteractive apt -yqq install wget cmake build-essential g++-arm-linux-gnueabihf 
+    DEBIAN_FRONTEND=noninteractive apt -yqq install wget cmake build-essential libicu-dev zlib1g-dev g++-arm-linux-gnueabihf 
 else
     apt -yqq update
     DEBIAN_FRONTEND=noninteractive apt -yqq install wget cmake build-essential libicu-dev zlib1g-dev
