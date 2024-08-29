41df8b300140834654b8a6ab086e724ebd58fb9d
Pasin Suriyentrakorn <pasin@couchbase.com>
2024-08-29 00:32:59 -0700

    Test Wins

--- desktop/scripts/verify_windows.sh
+++ desktop/scripts/verify_windows.sh
@@ -15,18 +15,22 @@ BUILD_DIR=$SCRIPT_DIR/../build
 rm -rf $CBL_LIB_DIR 2> /dev/null
 mkdir -p $CBL_LIB_DIR
 pushd $CBL_LIB_DIR > /dev/null
-ZIP_FILENAME=couchbase-lite-c-${EDITION}-${VERSION}-windows-x86_64.zip
-curl -L https://packages.couchbase.com/releases/couchbase-lite-c/${VERSION}/${ZIP_FILENAME} --output ./${ZIP_FILENAME}
+#ZIP_FILENAME=couchbase-lite-c-${EDITION}-${VERSION}-windows-x86_64.zip
+#curl -L https://packages.couchbase.com/releases/couchbase-lite-c/${VERSION}/${ZIP_FILENAME} --output ./${ZIP_FILENAME}
+
+ZIP_FILENAME=couchbase-lite-c-enterprise-windows-x86_64.zip
+cp "../bin/${ZIP_FILENAME}" .
+
 unzip ${ZIP_FILENAME}
 rm ${ZIP_FILENAME}
 popd > /dev/null
 
 mkdir -p $BUILD_DIR
 pushd $BUILD_DIR > /dev/null
-cmake -DCMAKE_PREFIX_PATH=$CBL_LIB_DIR/libcblite-$VERSION -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_PREFIX_PATH=$CBL_LIB_DIR/libcblite-3.2.0 -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --config Release
 pushd Release > /dev/null
-cp "$CBL_LIB_DIR/libcblite-${VERSION}/bin/cblite.dll" .
+cp "$CBL_LIB_DIR/libcblite-3.2.0/bin/cblite.dll" .
 ./verify.exe
 popd > /dev/null
 popd > /dev/null
